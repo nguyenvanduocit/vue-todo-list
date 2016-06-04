9f42999d48fc4964100812351e690cd4375316c9
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-06-04 19:34:06 +0700

    pharse 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,101 @@
-(function($, Vue, window){
+(function ($, Vue, window) {
+
+    var ListStore = {
+        state: {
+            items: [
+                {
+                    title: 'Mua sách cho con',
+                    color: '#F5A623'
+                }, {
+                    title: 'Đi chụp ảnh chùa Từ Tân',
+                    color: '#D0021B'
+                }, {
+                    title: 'Gọi điện cho ngoại',
+                    color: '#4A90E2'
+                },
+            ]
+        }
+    };
+
     var Header = Vue.extend({
-       template:$('#header-template').html()
+        template: $('#header-template').html(),
+        data: function () {
+            return {
+                listState: ListStore.state,
+                hoursRemain: 0
+            };
+        },
+        computed: {
+            hoursRemainPercent: function () {
+                return 100 - this.hoursRemain / 24 * 100;
+            },
+            taskRemain: function () {
+                return this.listState.items.length;
+            }
+        },
+        ready: function () {
+            var self = this;
+            self.calculateHoursRemain();
+            setInterval(function () {
+                self.calculateHoursRemain();
+            }, 1000);
+        },
+        methods: {
+            'calculateHoursRemain': function () {
+                var d = new Date();
+                this.hoursRemain = 24 - d.getHours();
+            }
+        }
     });
-var todolist = new Vue({
-    el:'#app',
-    components:{
-        'top-header':Header
-    },
-    template:$('#app-template').html()
-});
+
+    var Item = Vue.extend({
+        props: ['model'],
+        data: function () {
+            return {
+                isEditting: false
+            };
+        },
+        template: $('#item-template').html(),
+        methods: {
+            turnEditting: function () {
+                this.isEditting = true;
+                this.$nextTick(function(){
+                    $(this.$el).find('.input').focus();
+                });
+            },
+            save: function () {
+                this.isEditting = false;
+            },
+            done: function () {
+                this.$dispatch('item-deleted', this.model);
+            }
+        }
+    });
+
+    var List = Vue.extend({
+        props: ['collection'],
+        components: {
+            'item': Item
+        },
+        template: $('#list-template').html(),
+        events: {
+            'item-deleted': function (model) {
+                this.collection.$remove(model);
+            }
+        }
+    });
+
+    var todolist = new Vue({
+        el: '#app',
+        components: {
+            'top-header': Header,
+            'list': List,
+        },
+        data: function () {
+            return {
+                listState: ListStore.state,
+            };
+        },
+        template: $('#app-template').html()
+    });
 })(jQuery, Vue, window);
